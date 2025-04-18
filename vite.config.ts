9eecacbcec1efbd4e118b5a851bb3bc5579bd1a5
unggulmf <148017978+unggulmf@users.noreply.github.com>
2025-04-18 09:22:24 +0700

    Update vite.config.ts
    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:'/pemalang-laundry-bersih-ceria/',
   server: {
     host: "::",
     port: 8080,
